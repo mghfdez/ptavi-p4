cebc482dc29c4a5c659cb1f270d9596fa7d1d3bf
miguel angel fernandez sanchez <maferna@iota33.aulas.gsyc.es>
2014-10-20 16:05:37 +0200

    Gestion register y expires, responde codigos de error

--- server.py
+++ server.py
@@ -20,15 +20,30 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     def handle(self):    
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+        
         while 1:
-            line = self.rfile.read()
-            if line != "":
-                datos = line.split()
-                DICC_CLIENT[datos[3]] = self.client_address
-            else: 
+            cadena = self.rfile.read()
+            if cadena != "":
+                list_words = cadena.split()
+                if list_words[0] == 'REGISTER':
+                    correo = list_words[1]
+                    DICC_CLIENT[correo] = self.client_address
+                    print "Cliente añadido - " + list_words[1]
+                    if list_words[3] == 'Expires:':
+                        exp_time = int(list_words[4])
+                        print "Tiempo de expiración: " + str(exp_time)
+                        if exp_time == 0:
+                            del DICC_CLIENT[correo]
+                            print "Borrado " + correo
+                        print "\n", 
+                        print DICC_CLIENT
+                        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                    else:
+                        self.wfile.write("SIP/1.0 400 BAD REQUEST\r\n\r\n")
+                else:
+                    self.wfile.write("SIP/1.0 400 BAD REQUEST\r\n\r\n")
+            else:
                 break
-
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
