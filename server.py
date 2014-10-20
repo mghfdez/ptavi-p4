d94d7c17afed768b80290b2f76855f1f36126064
igarag <naxo.elderane@gmail.com>
2014-10-20 14:36:56 +0200

    Practica 4: Ejercicio 5

--- server.py
+++ server.py
@@ -9,15 +9,18 @@ import SocketServer
 
 SERVER_PORT = 6001
 ### Hereda de otra clase que se encarga de llenar el buffer, etc...INFO II
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        print "Cliente con IP " + str(self.client_address[0]) + " y puerto: ",
-        str(self.client_address[1])
+        print "Cliente con IP", str(self.client_address[0]), "y puerto:", 
+        print str(self.client_address[1])
+        Contenido = list(self.client_address)
+        print Contenido
+
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -33,7 +36,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     ### Crea una instancia UPDServer. Si dejas "" es localhost y EchoHandler el manejador
-    serv = SocketServer.UDPServer(("", SERVER_PORT), EchoHandler)
+    serv = SocketServer.UDPServer(("", SERVER_PORT), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     ### <<Quedate ahi por los tiempos de los tiempos escuchando>>
     serv.serve_forever()
