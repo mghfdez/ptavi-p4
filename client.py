f0f549713c73fba788b7cb8a660b0e330a9fdf8a
igarag <naxo.elderane@gmail.com>
2014-10-20 15:53:52 +0200

    Practica 4: Ejercicio 7

--- client.py
+++ client.py
@@ -9,29 +9,21 @@ import sys
 
 # Cliente UDP simple.
 
-# Toma por la linea de comandos IP y Puerto donde esta el servidor
+# ip | puerto | register | luke@polismassa.com
 SERVER = sys.argv[1]
-print SERVER
-#'localhost' # 127.0.0.1 
 PORT = int(sys.argv[2])
-print PORT
-#6001
-# Contenido que vamos a enviar tomado por la linea de comandos tambien.
-LINE = sys.argv[3]
-print LINE
-#'¡Hola mundo!'
+PETICIONES = sys.argv[3] 
+DIRECCION_SIP = sys.argv[4]
+EXPIRES = sys.argv[5]
 
+# Lo que manda el cliente es un conjunto de dos elementos, lo creamos concatenando:
+LINE = PETICIONES.upper() + " " + "sip:" + DIRECCION_SIP + " SIP/1.0\r\n"
+LINE = LINE + "Expires: " + EXPIRES + "\r\n\r\n"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-### -----------------------------------
-### Crea un objeto tipo socket  y le pasamos 2 parametros 
-### El primero un socket de internet y el segundo uno de datagramas (para UDP)
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-### Permite añadir mas opciones. Cuando se cierra el socket el OS cierra el puerto
-### inmediatamente y lo libera. El socket involucra 2 puertos, el mio y el del server (6001)
-### y el nuestro que lo abrimos nosotros. Con la linea le decimos: cuando cierres el socket
-### libera el puerto. 
+
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Lista que no puedes modifica.
+# Lista que no puedes modificar.
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
@@ -41,7 +33,6 @@ data = my_socket.recv(1024) #1024 => Tamaño del buffer.
 
 print 'Recibido -- ', data
 print "Terminando socket..."
-
 # Cerramos todo
 my_socket.close()
 print "Fin."
