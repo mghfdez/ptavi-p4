f31d2aaabf9825335778dc79c5e891aec91a5a42
miguel angel fernandez sanchez <maferna@iota34.aulas.gsyc.es>
2014-10-20 12:17:12 +0200

    peticiones SIP

--- client.py
+++ client.py
@@ -17,16 +17,24 @@ if len(datos_user) < 3:
 # Dirección IP del servidor.
 SERVER = datos_user[1]
 PORT = int(datos_user[2])
+METODO = datos_user[3]
+if METODO == 'register':
+    metod = "REGISTER"
+else:
+    metod = "unknown"
+LINE = SERVER + " " + str(PORT) + " " + metod
 
 # Contenido que vamos a enviar
-LINE = ' '.join(datos_user[3:])
-
+addr = datos_user[4]
+LINE = LINE + " " + "sip:" + addr
+VER = "SIP/1.0"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
+print "Enviando petición SIP..."
+LINE = LINE + " " + VER + "\r\n"
 my_socket.send(LINE + '\r\n')
 data = my_socket.recv(1024)
 
