fcf4ebb8dad3faa62ceaf2723ebecbcf2158ad6f
igarag <naxo.elderane@gmail.com>
2014-10-20 10:42:28 +0200

    Practica 4: Ejercicio 5

--- server.py
+++ server.py
@@ -7,7 +7,8 @@ en UDP simple
 
 import SocketServer
 
-
+SERVER_PORT = 6001
+### Hereda de otra clase que se encarga de llenar el buffer, etc...INFO II
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
@@ -15,16 +16,23 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        print "Cliente con IP|Puerto: " + str(self.client_address)
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
+            ### rfile.read para leer ---- wfile.write para escribir
             line = self.rfile.read()
-            print "El cliente nos manda " + line
             if not line:
                 break
+            else:
+                print "El cliente nos manda " + line
+            ### self.wfile.write(line) CON ESTA LINEA SI ES UN SERVIDOR DE ECHO
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    ### Crea una instancia UPDServer. Si dejas "" es localhost y EchoHandler el manejador
+    serv = SocketServer.UDPServer(("", SERVER_PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
+    ### <<Quedate ahi por los tiempos de los tiempos escuchando>>
     serv.serve_forever()
