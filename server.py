603d6128a7d3dea24811965048bdf038747ca86f
miguel angel fernandez sanchez <maferna@iota32.aulas.gsyc.es>
2014-10-22 12:33:52 +0200

    Ejercicio 8: escribe datos en fichero, falta tiempo

--- server.py
+++ server.py
@@ -14,10 +14,23 @@ if len(sys.argv) != 2:
 
 PORT = int(sys.argv[1])
 DICC_CLIENT = {}
+FILE = 'registered.txt'
+FICH = open(FILE, 'w')
+FICH.write("User \t IP \t Expires\r\n")
+FICH.close()
+
+def register2file(fichero, modo, usuario, host):
+    #Apunta en un txt cada vez que un usuario se registra o se da de baja
+    fich = open(fichero, 'a')
+    texto = usuario + '\t' + host + '\t' + "--fecha // hora-- " + modo + "\r\n"
+    fich.write(texto)
+    fich.close()
+
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+
     def handle(self):    
         # Escribe dirección y puerto del cliente (de tupla client_address)
         
@@ -28,23 +41,25 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 if list_words[0] == 'REGISTER':
                     correo = list_words[1]
                     DICC_CLIENT[correo] = self.client_address
+                    register2file(FILE, "Añadido", correo, self.client_address[0])
                     print "Cliente añadido - " + list_words[1]
                     if list_words[3] == 'Expires:':
                         exp_time = int(list_words[4])
                         print "Tiempo de expiración: " + str(exp_time)
                         if exp_time == 0:
+                            register2file(FILE, "Borrado", correo, self.client_address[0])
                             del DICC_CLIENT[correo]
                             print "Borrado " + correo
                         print "\n", 
                         print DICC_CLIENT
-                        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                        self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
                     else:
-                        self.wfile.write("SIP/1.0 400 BAD REQUEST\r\n\r\n")
+                        self.wfile.write("SIP/2.0 400 BAD REQUEST -- != expires\r\n\r\n")
                 else:
-                    self.wfile.write("SIP/1.0 400 BAD REQUEST\r\n\r\n")
+                    self.wfile.write("SIP/2.0 400 BAD REQUEST -- != register\r\n\r\n")
             else:
                 break
-
+     
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("",  PORT), SIPRegisterHandler)
