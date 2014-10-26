10a2c60c5f81af451c5bd8609347dfaaf1e7a640
lyanezgu <lyanezgu@gmail.com>
2014-10-26 11:22:15 +0100

    ejercicio6:envia register

--- client.py
+++ client.py
@@ -15,13 +15,14 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = sys.argv[3]
+REGISTER = sys.argv[3]
+DIRECTION = sys.argv[4]
+LINE = 'REGISTER sip:' + DIRECTION + ' SIP/2.0\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
-
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
 data = my_socket.recv(1024)
