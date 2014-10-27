3f857bc9017f28087acd4406b4b14fc5d8391133
igarag <naxo.elderane@gmail.com>
2014-10-27 11:00:03 +0100

    Practica4: Ejercicio 9 v1.0

--- client.py
+++ client.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente de UDP sencillo que abre un socket a un servidor
 """
 
 import socket
 import sys
 
-# Cliente UDP simple.
 
-# ip | puerto | register | luke@polismassa.com
+# CAMPOS => ip | puerto | register | luke@polismassa.com
 try:
-	SERVER = sys.argv[1]
-	PORT = int(sys.argv[2])
-	PETICIONES = sys.argv[3] 
-	DIRECCION_SIP = sys.argv[4]
-	EXPIRES = sys.argv[5]
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    PETICIONES = sys.argv[3]
+    DIRECCION_SIP = sys.argv[4]
+    EXPIRES = sys.argv[5]
 except IndexError:
-	print "Usage: client.py ip puerto register sip_address expires_value"
-# Lo que manda el cliente es un conjunto de dos elementos, lo creamos concatenando:
-LINE = PETICIONES.upper() + " " + "sip:" + DIRECCION_SIP + " SIP/1.0\r\n"
+    print "Usage: client.py ip puerto register sip_address expires_value"
+# Concatenamos un conjunto de dos elementos. Es lo que manda el cliente
+LINE = PETICIONES.upper() + " " + "sip:" + DIRECCION_SIP + " SIP/2.0\r\n"
 LINE = LINE + "Expires: " + EXPIRES + "\r\n\r\n"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,10 +27,11 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Lista que no puedes modificar.
 my_socket.connect((SERVER, PORT))
 
+print ""
 print "Enviando: " + LINE
 ### \r\n por convencion, retorno de carro - nueva linea.
 my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024) #1024 => Tamaño del buffer.
+data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
 print "Terminando socket..."
